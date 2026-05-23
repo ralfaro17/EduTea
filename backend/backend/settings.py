370af298685df60684f68b806644b41419b23966
Roger Alfaro <123581434+Ralfaro17@users.noreply.github.com>
2026-05-23 02:15:53 +0000

    fix changes in load times when rebuilding

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,6 +139,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
